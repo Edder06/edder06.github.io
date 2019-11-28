d7ebc29eabc85a12ba20355b03e61ef8e35e553e
Edder <yslaeder@hotmail.com>
2019-11-28 11:53:22 -0500

    borrando todo

--- public/admin/js/pages/table/editable_table_data.js
+++
@@ -1,8 +0,0 @@
-/**
- *  Document   : editable_table_data.js
- *  Author     : redstar
- *  Description: script for editable table data
- *
- **/
-"use strict";
- $('#mainTable').editableTableWidget();
